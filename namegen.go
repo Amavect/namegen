184477cf43a9c75e2d77ae72d36a9a5af78be921
Amavect <amavect@gmail.com>
2017-05-21 17:32:32 -0500

    initial commit

---
+++ namegen.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	//"time"
+)
+
+var (
+	f int = 2
+
+	consonant []string = []string{
+		"b",
+		"c",
+		"d",
+		"f",
+		"g",
+		"h",
+		"j",
+		"k",
+		"l",
+		"m",
+		"n",
+		"p",
+		"q",
+		"r",
+		"s",
+		"t",
+		"v",
+		"w",
+		"x",
+		"y",
+		"z",
+	}
+
+	vowel []string = []string{
+		"a",
+		"e",
+		"i",
+		"o",
+		"u",
+	}
+)
+
+func main() {
+	//rand.Seed(int64(time.Now().Nanosecond()*0))
+	
+	var length int = rand.Intn(8) + 5
+	var vow = false
+	var name string
+	
+	for ; length > 0; length-- {
+		if vow {
+			name = name + vowel[int(rand.Float64()*float64(len(vowel)))]
+		} else {
+			name = name + consonant[int(rand.Float64()*float64(len(consonant)))]
+		}
+		vow = !vow
+	}
+	fmt.Println(name)
+}
